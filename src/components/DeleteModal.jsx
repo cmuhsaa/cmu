81500386c996263b903d0e096f6b65122bdfbae8
Md Abdus Sabur <absabur929@gmail.com>
2025-08-27 10:54:16 +0600

    delete modal fix

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -2,7 +2,6 @@
 import React, { useEffect } from "react";
 
 export default function DeleteModal({ isOpen, onCancel, onDelete }) {
-  if (!isOpen) return null;
   useEffect(() => {
     if (isOpen) {
       // Scroll to top
